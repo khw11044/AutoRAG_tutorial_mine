8a2b2a94349db5e24cbe0da9cad697ad23e22c34
khw11044@gmail.com <khw11044@naver.com>
2024-07-11 14:04:51 +0900

    0711

--- make_qa.py
+++ make_qa.py
@@ -8,44 +8,25 @@ from llama_index.llms.openai import OpenAI
 from autorag.data.qacreation import generate_qa_llama_index, make_single_content_qa
 
 root_path = os.path.dirname(os.path.realpath(__file__))
-prompt = """다음은 걸그룹 뉴진스에 관한 기사입니다. 
-기사를 보고 할 만한 질문을 만드세요.
-반드시 뉴진스와 관련한 질문이어야 합니다.
-만약 주어진 기사 내용이 뉴진스와 관련되지 않았다면, 
-'뉴진스와 관련 없습니다.'라고 질문을 만드세요.
-
-기사:
-{{text}}
-
-생성할 질문 개수: {{num_questions}}
-
-예시:
-[Q]: 뉴진스는 몇 명인가요?
-[A]: 뉴진스는 총 다섯 명입니다.
-
-뉴진스와 관련이 없는 기사일 경우 예시:
-[Q]: 뉴진스와 관련 없습니다.
-[A]: 뉴진스와 관련 없습니다.
-
-결과:
-"""
+from prompt import prompt
 
 
 @click.command()
 @click.option('--corpus_path', type=click.Path(exists=True),
-              default=os.path.join('data', 'corpus_new.parquet'))
+              default=os.path.join('my_data', 'corpus_new.parquet'))
 @click.option('--save_path', type=click.Path(exists=False, dir_okay=False, file_okay=True),
-              default=os.path.join('data', 'qa_new.parquet'))
-@click.option('--qa_size', type=int, default=5)
+              default=os.path.join('my_data', 'qa_new.parquet'))
+@click.option('--qa_size', type=int, default=200)        # qa_size개의 실험 데이터셋 만들기
 def main(corpus_path, save_path, qa_size):
     load_dotenv()
 
     corpus_df = pd.read_parquet(corpus_path, engine='pyarrow')
-    llm = OpenAI(model='gpt-4o', temperature=0.5)
+    llm = OpenAI(model='gpt-4o', temperature=0.4)
+    
     qa_df = make_single_content_qa(corpus_df, content_size=qa_size, qa_creation_func=generate_qa_llama_index,
-                                   llm=llm, prompt=prompt, question_num_per_content=1)
-    # delete if the output question is '뉴진스와 관련 없습니다'
-    qa_df = qa_df.loc[~qa_df['query'].str.contains('뉴진스와 관련 없습니다')]
+                                   llm=llm, prompt=prompt, question_num_per_content=1)  # 하나의 컨텐츠 당 생성할 질문과 응답 수 
+    # 해당 자료와 관련이 없는 질문 및 응답 데이터는 제거 
+    qa_df = qa_df.loc[~qa_df['query'].str.contains('해당 자료와 관련이 없습니다.')]
     qa_df.reset_index(drop=True, inplace=True)
     qa_df.to_parquet(save_path)
 
